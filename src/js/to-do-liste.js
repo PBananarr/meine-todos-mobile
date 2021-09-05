5cce80753a4858213d90d8d2ae90f5735070edd5
PBananarr <79418608+PBananarr@users.noreply.github.com>
2021-09-05 17:33:20 +0200

    Add files via upload

---
+++ src/js/to-do-liste.js
@@ -0,0 +1,17 @@
+"use strict"
+
+
+
+let hinzufuegen = function() {
+    let eintrag = document.createElement("li");
+    let textNode = document.createTextNode(document.getElementById("eingabe").value);
+    eintrag.appendChild(textNode);
+    document.getElementById("aufgabenListe").appendChild(eintrag);
+    document.getElementById("eingabe").value = "";
+};
+
+let liste = document.getElementById("aufgabenListe");
+liste.addEventListener("click", function(event){
+event.target.classList.toggle("angeklickt");
+});
+
